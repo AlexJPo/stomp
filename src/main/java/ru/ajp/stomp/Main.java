9d6c8f6d94e1d7deb1e8e426494eba7d786b54c9
Alexey <swtorigra@gmail.com>
2018-02-22 10:41:21 +0300

    add stomp project

---
+++ src/main/java/ru/ajp/stomp/Main.java
@@ -0,0 +1,11 @@
+package ru.ajp.stomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
